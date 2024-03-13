df0d9c4ef14a437827d090df463dd2795f72e94c
NoW0NDER <145441591+NoW0NDER@users.noreply.github.com>
2024-03-14 00:19:58 +0800

    p1v1

--- p1.py
+++ p1.py
@@ -1,8 +1,156 @@
 import sys, random, grader, parse
 
+def to_string(problem):
+    current_map = problem.game_map
+    for key in problem.loc_map:
+        x, y = problem.loc_map[key]
+        current_map[x][y] = key
+    return '\n'.join([''.join(row) for row in current_map])
+
+
 def random_play_single_ghost(problem):
     #Your p1 code here
-    solution = ''
+    random.seed(problem.seed, version=1)
+    step_counter = 0
+    score  = 0
+    solution = f'seed: {problem.seed}\n'
+    solution += f"{step_counter}\n"
+    solution += to_string(problem) + '\n'
+    forbidden = {"%"}
+    
+    while problem.dot_count > 0:
+        # if step_counter > 50:
+        #     break
+        P_action_space = []
+        if problem.game_map[problem.loc_map['P'][0]][problem.loc_map['P'][1]+1] !="%":
+            P_action_space.append('E')
+        if problem.game_map[problem.loc_map['P'][0]-1][problem.loc_map['P'][1]] !="%":
+            P_action_space.append('N')
+        if problem.game_map[problem.loc_map['P'][0]+1][problem.loc_map['P'][1]] !="%":
+            P_action_space.append('S')
+        if problem.game_map[problem.loc_map['P'][0]][problem.loc_map['P'][1]-1] !="%":
+            P_action_space.append('W')
+        action = random.choice(P_action_space)
+        score -= 1
+        step_counter += 1
+        solution += f"{step_counter}: P moving {action}\n"
+        if action == 'E':
+            problem.loc_map['P'] = (problem.loc_map['P'][0], problem.loc_map['P'][1]+1)
+            if problem.game_map[problem.loc_map['P'][0]][problem.loc_map['P'][1]] == '.':
+                problem.dot_count -= 1
+                score+=10
+            if problem.game_map[problem.loc_map['P'][0]][problem.loc_map['P'][1]] == 'W':
+                problem.game_map[problem.loc_map['P'][0]][problem.loc_map['P'][1]-1] = ' '
+                problem.loc_map.pop('P')
+                score -= 500
+                solution += to_string(problem) + '\n'
+                solution += f"score: {score}\n"
+                solution += "WIN: Ghost"
+                break
+            problem.game_map[problem.loc_map['P'][0]][problem.loc_map['P'][1]] = 'P'
+            problem.game_map[problem.loc_map['P'][0]][problem.loc_map['P'][1]-1] = ' '
+        elif action == 'N':
+            problem.loc_map['P'] = (problem.loc_map['P'][0]-1, problem.loc_map['P'][1])
+            if problem.game_map[problem.loc_map['P'][0]][problem.loc_map['P'][1]] == '.':
+                problem.dot_count -= 1
+                score+=10
+            if problem.game_map[problem.loc_map['P'][0]][problem.loc_map['P'][1]] == 'W':
+                problem.game_map[problem.loc_map['P'][0]+1][problem.loc_map['P'][1]] = ' '
+                problem.loc_map.pop('P')
+                score -= 500
+                solution += to_string(problem) + '\n'
+                solution += f"score: {score}\n"
+                solution += "WIN: Ghost"
+                break
+            problem.game_map[problem.loc_map['P'][0]][problem.loc_map['P'][1]] = 'P'
+            problem.game_map[problem.loc_map['P'][0]+1][problem.loc_map['P'][1]] = ' '
+        elif action == 'S':
+            problem.loc_map['P'] = (problem.loc_map['P'][0]+1, problem.loc_map['P'][1])
+            if problem.game_map[problem.loc_map['P'][0]][problem.loc_map['P'][1]] == '.':
+                problem.dot_count -= 1
+                score+=10
+            if problem.game_map[problem.loc_map['P'][0]][problem.loc_map['P'][1]] == 'W':
+                problem.game_map[problem.loc_map['P'][0]-1][problem.loc_map['P'][1]] = ' '
+                problem.loc_map.pop('P')
+                score -= 500
+                solution += to_string(problem) + '\n'
+                solution += f"score: {score}\n"
+                solution += "WIN: Ghost"
+                break
+            problem.game_map[problem.loc_map['P'][0]][problem.loc_map['P'][1]] = 'P'
+            problem.game_map[problem.loc_map['P'][0]-1][problem.loc_map['P'][1]] = ' '
+        elif action == 'W':
+            problem.loc_map['P'] = (problem.loc_map['P'][0], problem.loc_map['P'][1]-1)
+            if problem.game_map[problem.loc_map['P'][0]][problem.loc_map['P'][1]] == '.':
+                problem.dot_count -= 1
+                score+=10
+            if problem.game_map[problem.loc_map['P'][0]][problem.loc_map['P'][1]] == 'W':
+                problem.game_map[problem.loc_map['P'][0]][problem.loc_map['P'][1]+1] = ' '
+                problem.loc_map.pop('P')
+                score -= 500
+                solution += to_string(problem) + '\n'
+                solution += f"score: {score}\n"
+                solution += "WIN: Ghost"
+                break
+            problem.game_map[problem.loc_map['P'][0]][problem.loc_map['P'][1]] = 'P'
+            problem.game_map[problem.loc_map['P'][0]][problem.loc_map['P'][1]+1] = ' '
+        solution += f"{to_string(problem)}\n"
+        if problem.dot_count == 0:
+            score += 500
+            solution += f"score: {score}\n"
+            solution += "WIN: Pacman"
+            break
+        solution += f"score: {score}\n"
+        W_action_space = []
+        if problem.game_map[problem.loc_map['W'][0]][problem.loc_map['W'][1]+1] not in forbidden:
+            W_action_space.append('E')
+        if problem.game_map[problem.loc_map['W'][0]-1][problem.loc_map['W'][1]] not in forbidden:
+            W_action_space.append('N')
+        if problem.game_map[problem.loc_map['W'][0]+1][problem.loc_map['W'][1]] not in forbidden:
+            W_action_space.append('S')
+        if problem.game_map[problem.loc_map['W'][0]][problem.loc_map['W'][1]-1] not in forbidden:
+            W_action_space.append('W')
+        action = random.choice(W_action_space)
+        step_counter += 1
+        solution += f"{step_counter}: W moving {action}\n"
+        if action == 'E':
+            problem.loc_map['W'] = (problem.loc_map['W'][0], problem.loc_map['W'][1]+1)
+            problem.game_map[problem.loc_map['W'][0]][problem.loc_map['W'][1]] = 'W'
+            problem.game_map[problem.loc_map['W'][0]][problem.loc_map['W'][1]-1] = ' '
+        elif action == 'N':
+            problem.loc_map['W'] = (problem.loc_map['W'][0]-1, problem.loc_map['W'][1])
+            problem.game_map[problem.loc_map['W'][0]][problem.loc_map['W'][1]] = 'W'
+            problem.game_map[problem.loc_map['W'][0]+1][problem.loc_map['W'][1]] = ' '
+        elif action == 'S':
+            problem.loc_map['W'] = (problem.loc_map['W'][0]+1, problem.loc_map['W'][1])
+            problem.game_map[problem.loc_map['W'][0]][problem.loc_map['W'][1]] = 'W'
+            problem.game_map[problem.loc_map['W'][0]-1][problem.loc_map['W'][1]] = ' '
+        elif action == 'W':
+            problem.loc_map['W'] = (problem.loc_map['W'][0], problem.loc_map['W'][1]-1)
+            problem.game_map[problem.loc_map['W'][0]][problem.loc_map['W'][1]] = 'W'
+            problem.game_map[problem.loc_map['W'][0]][problem.loc_map['W'][1]+1] = ' '
+        
+        if problem.loc_map['P'] == problem.loc_map['W']:
+            problem.loc_map.pop('P')
+            solution += f"{to_string(problem)}\n"
+            score -= 500
+            solution += f"score: {score}\n"
+            solution += "WIN: Ghost"
+            break
+        else:
+            solution += f"{to_string(problem)}\n"
+            solution += f"score: {score}\n"
+        
+            
+            
+                
+        
+            
+    
+    
+    
+    
+    
     return solution
 
 if __name__ == "__main__":
--- parse.py
+++ parse.py
@@ -1,8 +1,42 @@
 import os, sys
 
+class Problem:
+    def __init__(self, seed, game_map, loc_map,dot_count,dots):
+        self.seed = seed
+        self.game_map = game_map
+        self.loc_map = loc_map
+        self.dot_count = dot_count
+        self.dots = dots
+
 def read_layout_problem(file_path):
     #Your p1 code here
-    problem = ''
+    
+    with open(file_path, 'r') as file:
+        seed = int(file.readline().strip().split()[1])
+        game_map = []
+        loc_map = dict()
+        dot_count = 0
+        dots = set()
+        while True:
+            line = file.readline().strip()
+            if not line:
+                break
+            
+            game_map.append([])
+            for token in line:
+                if token == '%':
+                    game_map[-1].append("%")
+                elif token == '.':
+                    game_map[-1].append(".")
+                    dot_count += 1
+                    dots.add((len(game_map)-1, len(game_map[-1])-1))
+                elif token == " ":
+                    game_map[-1].append(" ")
+                else:
+                    game_map[-1].append(" ")
+                    loc_map[token] = (len(game_map)-1, len(game_map[-1])-1)
+    
+        problem = Problem(seed, game_map, loc_map,dot_count,dots)
     return problem
 
 if __name__ == "__main__":
